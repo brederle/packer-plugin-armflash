d7bbe0f96a7f14932132680b60abc5ecdf830dd1
brederle <rbe-github@t-online.de>
2024-08-16 17:46:11 +0200

    Upgrade to packer 1.11.x plugin rules and dependencies

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
 	"github.com/brederle/packer-plugin-armflash/builder"
+	"github.com/brederle/packer-plugin-armflash/version"
 )
+
 
 func main() {
     pluginSet := plugin.NewSet()
 	pluginSet.RegisterBuilder(plugin.DEFAULT_NAME, builder.NewBuilder())
     //pluginSet.RegisterPostProcessor("flasher", flasher.newPostProcessor())
+	pluginSet.SetVersion(version.PluginVersion)
     err := pluginSet.Run()
     if err != nil {
         fmt.Fprintln(os.Stderr, err.Error())
---
+++ version/version.go
@@ -0,0 +1,11 @@
+
+package version
+
+import "github.com/hashicorp/packer-plugin-sdk/version"
+
+var (
+	Version           = "1.1.0"
+	VersionPrerelease = ""
+	VersionMetadata   = ""
+	PluginVersion     = version.NewPluginVersion(Version, VersionPrerelease, VersionMetadata)
+)
